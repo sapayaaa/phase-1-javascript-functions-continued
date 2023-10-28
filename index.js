aab9d25e6f96bbcdc8dc897cf7cb758e540a05fe
Spy <Spyngunga@gmail.com>
2023-10-28 07:24:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaultActivity = "roller-skate"){
+  return `This Saturday, I want to ${defaultActivity}!`
+}
+
+function mondayWork(defaultActivity = `go to the office`) {
+  return `This Monday, I will ${defaultActivity}.`
+}
+
+function wrapAdjective(sirName = "!!") {
+  return function(adjective = "a dedicated programmer"){
+      return `You are ${sirName}${adjective}${sirName}!`
+  }
+}
